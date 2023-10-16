df2a303006d78289aa5ad9f7918e334fb01a0950
Miranda Foster <148116726+mirabfost@users.noreply.github.com>
2023-10-16 13:18:24 -0400

    project upload

---
+++ find.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "./includes/header.html" ?>
+
+
+<div class="content-container">
+    <header id="header-alt">
+        <div class="crumbs">
+            <div class="crumbs-items">
+                <a href="index">Home</a> &gt; <a href="find">Find a Food Bank</a>
+            </div>
+    </header>
+
+    <section class="content-1" id="map">
+        <div class="map-container">
+            <div class="map-title"><h1>Nearby Foodbanks</div>
+            <div class="map-item1">
+    <img width="600" src="https://maps.googleapis.com/maps/api/staticmap?center=500+W+Livingston+St,+Orlando,+FL+32801&zoom=13&scale=2&size=600x300&maptype=roadmap&key=AIzaSyBZto6nV4B8dvzOJjZbJm7qK6FTooBvs8c&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xfffb00%7Clabel:1%7C18+N+Terry+Ave,+Orlando,+FL+32801&markers=size:mid%7Ccolor:0xfffb00%7Clabel:3%7C624+Lexington+Ave,+Orlando,+FL+32801&markers=size:mid%7Ccolor:0xfffb00%7Clabel:5%7C604+Jackson+St,+Orlando,+FL+32805&markers=size:mid%7Ccolor:0xfffb00%7Clabel:2%7C701+Concord+St,+Orlando,+FL+32805&markers=size:mid%7Ccolor:0xfffb00%7Clabel:7%7C411+Mercy+Dr,+Orlando,+FL+32805&markers=size:mid%7Ccolor:0xff2600%7Clabel:%7C500+W+Livingston+St,+Orlando,+FL+32801&markers=size:mid%7Ccolor:0xfffb00%7Clabel:4%7C808+W+Central+Blvd,+Orlando,+FL+32805&markers=size:mid%7Ccolor:0xfffb00%7Clabel:6%7C1310+W+Colonial+Dr+#25,+Orlando,+FL+32804" alt="Google Map of 500 W Livingston St, Orlando, FL 32801">
+</div>
+<div class="map-item2">
+    <h4>1. Coalition for the Homeless</h4>
+    <p>18 N Terry Ave, Orlando, FL 32801</p>
+    <p>Phone: (407)-426-1250</p>
+
+    <h4>2. Emmanuel United Methodist Church</h4>
+    <p>701 Concord St, Orlando, FL 32805</p>
+    <p>Phone: (321)-236-0500</p>
+
+    <h4>3. Salvation Army Men’s Shelter</h4>
+    <p>624 Lexington Ave, Orlando, FL 32801</p>
+    <p>Phone: (407)-423-8581</p>
+
+    <h4>4. Christian Service Center</h4>
+    <p>808 W Central Blvd, Orlando, FL 32805</p>
+    <p>Phone: (407)-425-2523</p>
+
+    <h4>5. Shiloh Baptist Church of Orlando</h4>
+    <p>604 Jackson St, Orlando, FL 32805</p>
+    <p>Phone: (407)-422-9631</p>
+
+    <h4>6. Central Florida Haven of Hope</h4>
+    <p>1310 W Colonial Dr #25, Orlando, FL 32804</p>
+    <p>Phone: (407)-298-2056</p>
+
+    <h4>7. Second Harvest</h4>
+    <p>411 Mercy Dr, Orlando, FL 32805</p>
+    <p>Phone: (407)-295-1066</p>
+</div>
+</div>
+
+    </section>
+
+    
+</div>
+
+<div class="map-search">
+    <div class="map-search-header">
+<h1>Food Banks of Central Florida</h1>
+</div>
+<div class="map-search-block1">
+<p><a href="https://studentunion.ucf.edu/knights-pantry/" target=_blank>Knights Helping Knights Pantry</a></p>
+<p>4133 Pyxis Ln, Orlando, FL 32816</p>
+<p>(407) 823-3663</p>
+
+<p><a href="https://www.feedhopenow.org/site/SPageServer/" target=_blank>feedhSecond Harvest Food Bank</a></p>
+<p> 411 Mercy Dr, Orlando, FL 32805</p>
+<p>(407) 295-1066</p>
+
+<p><a href="https://www.christianservicecenter.org/" target=_blank >Christian Service Center</a></p>
+<p>808 W Central Blvd, Orlando, FL 32805</p>
+<p>(407) 425-2523</p>
+</div>
+
+<div class="map-search-block2">
+<p><a href="http://iscf.org/social-services-2/seeds-of-paradise-food-pantry/" target=_blank >Seeds Of Paradise Food Pantry</a></p>
+<p>1460 N Goldenrod Rd, Orlando, FL 32807</p>
+<p>(407) 250-5882</p>
+
+<p><a href="https://www.cflhavenofhope.com/" target=_blank>Central Florida Haven of Hope</a></p>
+<p>1310 W Colonial Dr #25, Orlando, FL 32804</p>
+<p>(407) 298-2056</p>
+
+<p><a href="https://unitedagainstpoverty.org/" target=_blank>United Against Poverty</a></p>
+<p>150 W Michigan St, Orlando, FL 32806</p>
+<p>(407) 650-0774</p>
+</div>
+
+<div class="map-search-block3">
+<p><a href="http://sbcorlando.org/" target=_blank>Shiloh Baptist Church of Orlando</a></p>
+<p>604 Jackson St, Orlando, FL 32805</p>
+<p>(407) 422-9631</p>
+
+<p><a href="https://cflcc.org/mission-market-orlando/" target=_blank>Orlando Mission Market</a></p>
+<p>1771 N Semoran Blvd, Orlando, FL 32807</p>
+<p>(407) 658-1818</p>
+
+<p><a href="https://www.oneheartforwomenandchildren.org/" target=_blank>One Heart For Women And Children</a></p>
+<p> 2040 N Rio Grande Ave, Orlando, FL 32804</p>
+<p>(321) 299-4594</p>
+</div>
+
+</div>
+<?php include "./includes/footer.html" ?>
+
+
+</div>
+</body>
+</html>
